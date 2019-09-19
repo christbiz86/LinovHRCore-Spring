fc87de50410fef1a7bb1bc3a23c9b2829750fecc
ajiprays <ajiprays@users.noreply.github.com>
2019-09-19 10:12:19 +0700

    lovs

---
+++ src/main/java/com/demo/model/LovTypes.java
@@ -0,0 +1,109 @@
+package com.demo.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table(name = "core_lov_type")
+public class LovTypes {
+
+	@Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private String id;
+
+    @Column(name = "code")
+    private String code;
+    
+    @Column(name = "name")
+    private String name;
+    
+    @Column(name = "created_by")
+    private Integer createdBy;
+
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+7")
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+
+    @Column(name = "updated_by")
+    private Integer updatedBy;
+
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+7")
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    @Column(name = "arg1")
+    private String arg1;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(Integer createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Timestamp getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Timestamp createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Integer getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(Integer updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	public Timestamp getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Timestamp updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public String getArg1() {
+		return arg1;
+	}
+
+	public void setArg1(String arg1) {
+		this.arg1 = arg1;
+	}
+}
---
+++ src/main/java/com/demo/model/Lovs.java
@@ -0,0 +1,146 @@
+package com.demo.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table(name = "core_lovs")
+public class Lovs {
+
+	@Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private String id;
+
+    @JoinColumn(name = "lov_type_id", referencedColumnName = "id")
+    @OneToOne
+    private LovTypes lovTypes;
+    
+    @Column(name = "key_data")
+    private String keyData;
+
+    @Column(name = "val_data")
+    private String valData;
+    
+    @Column(name = "created_by")
+    private Integer createdBy;
+
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+7")
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+
+    @Column(name = "updated_by")
+    private Integer updatedBy;
+
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+7")
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+    
+    @Column(name = "is_disableable")
+    private String isDisableable;
+    
+    @Column(name = "is_active")
+    private String isActive;
+    
+    @Column(name = "arg1")
+    private String arg1;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public LovTypes getLovTypes() {
+		return lovTypes;
+	}
+
+	public void setLovTypes(LovTypes lovTypes) {
+		this.lovTypes = lovTypes;
+	}
+
+	public String getKeyData() {
+		return keyData;
+	}
+
+	public void setKeyData(String keyData) {
+		this.keyData = keyData;
+	}
+
+	public String getValData() {
+		return valData;
+	}
+
+	public void setValData(String valData) {
+		this.valData = valData;
+	}
+
+	public Integer getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(Integer createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Timestamp getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Timestamp createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Integer getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(Integer updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	public Timestamp getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Timestamp updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public String getIsDisableable() {
+		return isDisableable;
+	}
+
+	public void setIsDisableable(String isDisableable) {
+		this.isDisableable = isDisableable;
+	}
+
+	public String getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(String isActive) {
+		this.isActive = isActive;
+	}
+
+	public String getArg1() {
+		return arg1;
+	}
+
+	public void setArg1(String arg1) {
+		this.arg1 = arg1;
+	}
+    
+}
