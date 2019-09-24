5da4bab297f0f61ebf60c78d9c84cbb6963b09d9
irwanbekasi1993 <irwanbekasi1993@gmail.com>
2019-09-24 10:21:00 +0700

    devirwan : update penggunaan BaseEntity 24/09/2019

--- src/main/java/com/demo/model/Company.java
+++ src/main/java/com/demo/model/Company.java
@@ -1,43 +1,28 @@
 package com.demo.model;
 
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name="core_companies")
-public class Company extends BaseEntity{
-
-//    @Id
-//    @Column(name = "id")
-//    @GenericGenerator(name="UUID", strategy="org.hibernate.id.UUIDGenerator")
-//    private String id;
+public class Company extends BaseEntity {
 
     @Column(name = "name")
     private String name;
-
-//    @OneToOne(mappedBy = "company",fetch = FetchType.EAGER)
-//    private Costcenter costcenters;
 
     @JsonIgnoreProperties(value = {"companies"})
     @ManyToOne(optional = false,fetch = FetchType.EAGER)
     @JoinColumn(name="tenant_id", referencedColumnName = "id")
     private Tenant tenant;
-
-//    public String getId(){
-//        return id;
-//    }
-//
-//    public void setId(String id){
-//        this.id = id;
-//    }
 
     public String getName(){
         return name;
@@ -46,14 +31,6 @@ public class Company extends BaseEntity{
     public void setName(String name){
         this.name = name;
     }
-
-//    public Costcenter getCostcenters() {
-//        return costcenters;
-//    }
-//
-//    public void setCostcenters(Costcenter costcenters) {
-//        this.costcenters = costcenters;
-//    }
 
     public Tenant getTenant() {
         return tenant;
