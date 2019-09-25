94fabbc46878d65c58a3428304f2a117e57d4541
irwanbekasi1993 <irwanbekasi1993@gmail.com>
2019-09-25 08:34:41 +0700

    update fix error di java

--- src/main/java/com/demo/dao/ProvinceDao.java
+++ src/main/java/com/demo/dao/ProvinceDao.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public class ProvinceDao extends AbstractJpaDao<Province> {
+public class ProvinceDao extends ParentDao {
 
     public List<Province> findAll(){
         return super.entityManager.createQuery("FROM Province").getResultList();
