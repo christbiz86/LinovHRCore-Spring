0a22756e1653d04a1e13f9d47b81ffb2617b80f8
mnurdin <muhamadnurdin141@gmail.com>
2019-09-26 09:59:20 +0700

    add complocation

--- src/main/java/com/demo/model/Location.java
+++ src/main/java/com/demo/model/Location.java
@@ -1,22 +1,14 @@
 package com.demo.model;
 
-import java.io.Serializable;
-import java.sql.Timestamp;
-
 import javax.persistence.Cacheable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 import org.hibernate.annotations.CacheConcurrencyStrategy;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 @Cacheable
