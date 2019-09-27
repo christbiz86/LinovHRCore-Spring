c573394c9c2812db36082b59049acd818faa2c89
Hermawan <minghermawan@yahoo.com>
2019-09-27 14:30:51 +0700

    Set SecurityFilter

--- src/main/java/com/demo/core/SecurityFilter.java
+++ src/main/java/com/demo/core/SecurityFilter.java
@@ -11,35 +11,37 @@ import com.demo.helper.JwtAuth;
 public class SecurityFilter extends OncePerRequestFilter {
 
 
+	private void authentication(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+    	try {
+    		String token = request.getHeader("Authorization").replace("Bearer ", "");
+    		String username = JwtAuth.getUsername(token);
+            if(username == null) {
+            	throw new NullPointerException();
+            }
+    		String password = JwtAuth.getPassword(token);
+            if(password == null) {
+            	throw new NullPointerException();
+            }
+            filterChain.doFilter(request, response);        	
+    	}
+    	catch(NullPointerException e) {
+    		response.setStatus(403);
+    		response.getWriter().write("Unauthorized");
+    		response.getWriter().flush();
+    		response.getWriter().close();
+    	}
+		
+	}
+
+
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-    	String uri = request.getRequestURI();
-        System.out.println(uri);
-        if( uri.equals("/signin") || uri.equals("/user") ) {
-            filterChain.doFilter(request, response);
-        } else {
-        	try {
-        		String token = request.getHeader("Authorization").replace("Bearer ", "");
-        		String username = JwtAuth.getUsername(token);
-                if(username == null) {
-                	throw new NullPointerException();
-                }
-        		String password = JwtAuth.getPassword(token);
-                if(password == null) {
-                	throw new NullPointerException();
-                }
-                filterChain.doFilter(request, response);        	
-        	}
-        	catch(NullPointerException e) {
-        		response.setStatus(403);
-        		response.getWriter().write("Unauthorized");
-        		response.getWriter().flush();
-        		response.getWriter().close();
-        	}
-        }
-		
+		final String uri = request.getRequestURI();
+		System.out.println(uri);
+		filterChain.doFilter(request, response);
 	}
 
 
