2f4941af0ce8ae3efdc944fad66200c7e7bdb8f5
marcato21 <40853349+marcato21@users.noreply.github.com>
2019-09-30 09:12:42 +0700

    tambah LOV

--- Scripts/Script.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO public.core_lovs
-(lov_type_id, key_data, val_data, created_by, created_at, is_disableable, is_active, arg1, "version")
-VALUES('3e9d6b81-8718-4d94-bc79-efa9ada23f86', 'INS', 'Instructor', 0, current_timestamp, false, true, NULL, 0);
