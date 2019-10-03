e0eaea3f6a4540dcccbb512831c5888476726952
irwanbekasi1993 <irwanbekasi1993@gmail.com>
2019-10-03 16:08:15 +0700

    i

--- src/main/java/com/demo/service/JobService.java
+++ src/main/java/com/demo/service/JobService.java
@@ -1,9 +1,5 @@
 package com.demo.service;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
